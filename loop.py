dcee75fd7e77e9722ce908ec2cdc87b563283beb
jee936 <60419300+jee936@users.noreply.github.com>
2020-01-29 12:32:36 +0530

    Create loop.py
    type the numbers in enter the number area  
    whenever entering the number the loop exists and when the alphabets have entered the loop stops

---
+++ loop.py
@@ -0,0 +1,12 @@
+a = " "
+while not(a == 'q' or a == 'Q'):
+    a = input("enter a number")
+    
+    
+    '''
+    sample output:
+    enter a number 4
+    enter a number 5
+    enter a number q
+    exit
+    '''
